97dfb8513ba83b0aebedfb98e8a17ef8864cd545
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-06-23 22:07:14 -0400

    Create main.py
    Main PY

---
+++ Consolidation OOP/main.py
@@ -0,0 +1,22 @@
+from consolidate import *
+import glob
+import matplotlib.pyplot as plt
+import pdb
+from PIL import Image
+
+
+
+cwd = os.getcwd()
+#All the data from deck.yaml is now in the following deck variable
+deck = Deck( cwd + "/deck.yaml" )
+
+meshes = Meshes( deck )
+
+model = HeatTransfer(deck)
+
+plots=Plots(deck,meshes,meshes.T)
+
+solves = Solves( deck,model,meshes,plots)
+
+animates=Animates()
+
