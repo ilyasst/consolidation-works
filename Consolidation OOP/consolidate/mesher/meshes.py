7c1112d7a29e08a995f84db3f706780574c71ae5
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-06-23 22:09:50 -0400

    Create meshes.py
    Mesher... X axis is in columns and Y axis is in rows

---
+++ Consolidation OOP/consolidate/mesher/meshes.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pdb
+
+class Meshes():
+
+    def __init__(self, deck):
+        self.deck = deck
+        self.set_mesh_grid()
+        self.init_mesh()
+    
+    def set_mesh_grid(self):
+        self.nx = int(float(self.deck.doc["Simulation"]["lenX"])/float(self.deck.doc["Simulation"]["dx"]))
+        self.ny = int(float(self.deck.doc["Simulation"]["lenY"])/float(self.deck.doc["Simulation"]["dy"]))
+        X, Y = np.meshgrid(np.arange(0, self.ny), np.arange(0, self.nx))
+        X=X[1,:].copy()
+        Y=Y[:,1].copy()
+        self.X = X
+        self.Y = Y
+
+    def init_mesh(self):
+        self.nx1, self.nx2 = self.nx, self.nx
+        self.ny1, self.ny2 = int(self.ny/2), self.ny
+        
+        T = np.zeros((self.ny, self.nx))        
+        T[0:self.ny1, 0:self.nx1] = self.deck.doc["Materials"]["Material1"]["Domain Initial Temperature"] # Set array size and set the interior value with Tini
+        T[self.ny1:self.ny2, 0:self.nx2] = self.deck.doc["Materials"]["Material2"]["Domain Initial Temperature"] # Set array size and set the interior value with Tini
+        T[int(self.ny/2), int(0.2*self.nx):int(0.8*self.nx+1)] = 100
+        T[int(self.ny/2-1), int(0.2*self.nx):int(0.8*self.nx+1)] = 100
+        self.T = T.copy()
+        self.T0=T.copy()
+        
+        
+        DiffTotalX = np.zeros((self.ny, self.nx)) 
+        DiffTotalX[0:self.ny1, 0:self.nx1] = self.deck.doc["Materials"]["Material1"]["Thermal Diffusivity X"]
+        DiffTotalX[self.ny1:self.ny2, 0:self.nx2] = self.deck.doc["Materials"]["Material2"]["Thermal Diffusivity X"]
+
+        DiffTotalY = np.zeros((self.ny, self.nx)) 
+        DiffTotalY[0:self.ny1, 0:self.nx1] = self.deck.doc["Materials"]["Material1"]["Thermal Diffusivity Y"]
+        DiffTotalY[self.ny1:self.ny2, 0:self.nx2] = self.deck.doc["Materials"]["Material2"]["Thermal Diffusivity Y"]
+        self.DiffTotalX = DiffTotalX
+        self.DiffTotalY = DiffTotalY
