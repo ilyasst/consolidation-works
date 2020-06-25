b90d1b24fdcf46b587381607e49e813dc4de7fc3
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-06-24 20:24:10 -0400

    Update animates.py
    animates now receives deck as input, since the .GIF will be saved in the direction determined by deck.yaml

--- Consolidation OOP/consolidate/animator/animates.py
+++ Consolidation OOP/consolidate/animator/animates.py
@@ -4,20 +4,22 @@ Created on Wed Jun 10 19:35:49 2020
 
 @author: andre
 """
-
-
 from PIL import Image
 import glob
+
+
 class Animates:
     
-    def __init__(self):
+    def __init__(self,deck):
+        self.deck=deck
         self.do_animation()
         
         
         
     def do_animation(self):   
         frames = []
-        imgs = glob.glob("./output/*.jpg")
+        # imgs = glob.glob("./output/*.jpg")
+        imgs = glob.glob(self.deck.plot_dir + '*.jpg')
         for i in imgs:
             new_frame = Image.open(i)
             frames.append(new_frame)
@@ -26,7 +28,8 @@ class Animates:
         
          
         # Save into a GIF file that loops forever
-        frames[0].save('./output/temperature.gif', format='GIF',
+        direction=(self.deck.plot_dir+self.deck.doc["Animation"]["name"]+'.gif')
+        frames[0].save(direction, format='GIF',
                         append_images=frames[1:],
                         save_all=True,
                         duration=200, loop=0)
