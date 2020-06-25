12e9d317040a2e7f2075b185cf6b590e68ff011f
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-06-24 20:18:44 -0400

    Update main.py
    Animates has now "deck" as input

--- Consolidation OOP/main.py
+++ Consolidation OOP/main.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 
 cwd = os.getcwd()
-#All the data from deck.yaml is now in the following deck variable
+# All the data from deck.yaml is now in the following deck variable
 deck = Deck( cwd + "/deck.yaml" )
 
 meshes = Meshes( deck )
@@ -18,5 +18,4 @@ plots=Plots(deck,meshes,meshes.T)
 
 solves = Solves( deck,model,meshes,plots)
 
-animates=Animates()
-
+animates=Animates(deck)
