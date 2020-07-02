39c58655af9af72fcfe212341dfa02c27df766c9
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-07-02 15:14:29 -0400

    Update intimate_contact.py
    before, viscosity in the borders were not calculated.. Calculation of the borders is now included

--- Consolidation OOP/consolidate/models/intimate_contact.py
+++ Consolidation OOP/consolidate/models/intimate_contact.py
@@ -9,11 +9,19 @@ import numpy as np
 
 class IntimateContact():
     
-        
+    # def __init__(self, deck):
+    #         self.deck=deck
         
     def viscosity_timestep(self,v,u):
              
-          v[1:-1, 1:-1]=1.14*10**(-12)*np.exp(26300/u[1:-1, 1:-1])
+          v[0:, 0:]=1.14*10**(-12)*np.exp(26300/u[0:,0:])
           return v
      
+        
+    def dic_timestep(self, dic, dic0, v, t):
+        C1=5*(1+0.45)*(0.85)**2
+        dic[0:,0:]=dic0[0:,0:]*((1+(C1-0.65)*(5*10**5/v[0:,0:])*t)**(1/5))
+        
+        return dic
+        
 
