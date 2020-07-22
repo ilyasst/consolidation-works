6397a16ee27fc0a2d4d1323d26cc06e5d6f3e269
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-07-22 08:30:28 -0400

    Update deck.py

--- consolidate/IO/deck.py
+++ consolidate/IO/deck.py
@@ -19,9 +19,10 @@ class Deck():
     def create_folder_structure(self):        
         self.plot_dirTemp = self.doc["Plot"]["Temp Output Folder"]
         self.plot_dirDic = self.doc["Plot"]["Dic Output Folder"]
-        check_folder = os.path.isdir(self.plot_dirTemp)
-        check_folder = os.path.isdir(self.plot_dirDic)
-        if not check_folder:
+        check_folder_Temp = os.path.isdir(self.plot_dirTemp)
+        check_folder_Dic = os.path.isdir(self.plot_dirDic)
+        if not check_folder_Temp:
               os.makedirs(self.plot_dirTemp)
+        if not check_folder_Dic:       
               os.makedirs(self.plot_dirDic)
 
