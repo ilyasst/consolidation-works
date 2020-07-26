3c80c080527bb692ab9a0529b8e5adbc35cbe65f
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-07-26 00:40:20 -0400

    Update main.py
    included geometry

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from consolidate import *
 
 
 
-
 cwd = os.getcwd()
 # All the data from deck.yaml is now in the following deck variable
 
 deck = Deck( cwd + "/TwoPlates.yaml" )
 
-meshes = MeshTwoPlates( deck )
+geometry = Geometry(deck)
+
+meshes = MeshTwoPlates( deck,geometry )
 
 model_HT= HeatTransfer(deck,meshes)
 
