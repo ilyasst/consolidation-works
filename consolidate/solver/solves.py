b7d704c3a2fb86ec9ff475d6a01fdfbe29b1a401
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-11 09:14:49 -0400

    Update solves.py
    included convection in the solver
    .meshes were replaced by .BC, since the conditions are now written in BC and not in mashes anymore

--- consolidate/solver/solves.py
+++ consolidate/solver/solves.py
@@ -4,12 +4,12 @@ import numpy as np
 
 class SolvesTwoPlates:
     
-    def __init__(self, deck,model_HT,meshes,plots,model_IC):
+    def __init__(self, deck,meshes,BC,model_HT,model_IC,plots):
         self.deck = deck
-        # Compute the TEMPERATURE for each node
-        self.model_HT = model_HT
+        self.meshes=meshes
+        self.BC=BC
+        self.model_HT=model_HT
         self.model_IC=model_IC
-        self.meshes=meshes  
         self.plots = plots
         self.do_solver()
         
@@ -19,27 +19,27 @@ class SolvesTwoPlates:
         
     def do_solver(self):
         for m in range(int(self.deck.doc["Simulation"]["Number Time Steps"])):
-# -------------- CALCULATE TEMPERATURE FOR EACH STEP INCREMENT----------             
-            self.meshes.T0, self.meshes.T = self.model_HT.do_timestep(self.meshes.T0, self.meshes.T, self.meshes.DiffTotalX, self.meshes.DiffTotalY, self.meshes.Q)
+# -------------- CALCULATE TEMPERATURE FOR EACH STEP INCREMENT----------        
+            self.BC.T0, self.BC.T = self.model_HT.do_convection(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy, self.BC.Q)            
+            self.BC.T0, self.BC.T = self.model_HT.do_timestep(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy, self.BC.Q)
 # -------------- FORCE TEMPERATURE AT THE INTERFACE: ISOTHERMAL CONDITION----------             
-            self.meshes.T[int(self.meshes.ny/2), 1:-1] = self.deck.doc["Processing Parameters"]["Temperature"]
-            self.meshes.T[int(self.meshes.ny/2-1), 1:-1] = self.deck.doc["Processing Parameters"]["Temperature"]
+            # self.BC.T[50, 1:-1] = self.deck.doc["Boundary Condition"]["Ideal Temperature"]
 # -------------- UPDATE T0----------             
-            self.meshes.T0=self.meshes.T.copy()
+            self.BC.T0=self.BC.T.copy()
 # -------------- CALCULATE VISCOSITY FOR EACH STEP INCREMENT----------             
-            self.meshes.Visc=self.model_IC.viscosity_timestep(self.meshes.Visc, self.meshes.T)
-# -------------- CALCULATE Dic FOR EACH STEP INCREMENT----------             
-            self.meshes.Dic=self.model_IC.dic_timestep(self.meshes.Dic, self.meshes.Dic0, self.meshes.Visc, float(self.deck.doc["Simulation"]["Time Step"]))
-# -------------- UPDATE THE INTEGRAL----------             
-            self.model_IC.aux=self.model_IC.update_aux( float(self.deck.doc["Simulation"]["Time Step"]), self.meshes.Visc)
-# -------------- UPDATE Dic----------             
-            self.meshes.Dic=np.clip(self.meshes.Dic,0,1)
+            self.BC.Visc=self.model_IC.viscosity_timestep(self.BC.Visc, self.BC.T)
+# # -------------- CALCULATE Dic FOR EACH STEP INCREMENT----------             
+            self.BC.Dic=self.model_IC.dic_timestep(self.BC.Dic, self.BC.Dic0, self.BC.Visc, float(self.deck.doc["Simulation"]["Time Step"]))
+# # -------------- UPDATE THE INTEGRAL----------             
+            self.model_IC.aux=self.model_IC.update_aux( float(self.deck.doc["Simulation"]["Time Step"]), self.BC.Visc)
+# # -------------- UPDATE Dic----------             
+            self.BC.Dic=np.clip(self.BC.Dic,0,1)
 # -------------- DO PLOT ACCORDING TO THE SELECTED INTERVAL----------             
             if m in self.plots.mfig:
-                self.plots.update_Dic(self.meshes.Dic)
-                self.plots.update_T(self.meshes.T)       
+                self.plots.update_Dic(self.BC.Dic)
+                self.plots.update_T(self.BC.T)       
                 self.plots.do_plots(m)    
         self.plots.do_animation()
-        self.model_IC.calculate_average_dic()
+        # self.model_IC.calculate_average_dic()
             
      
