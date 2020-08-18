9f624a3489db84cf76d878f44e8b09ab30a45c17
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-18 14:01:40 -0400

    Update mesh.py
    just added a matrix M with dimentions XY. Still it is not being used, but maybe in a near future

--- consolidate/mesher/mesh.py
+++ consolidate/mesher/mesh.py
@@ -4,11 +4,10 @@ class Mesh:
 
     def __init__(self, deck, domain):
         self.set_mesh_grid(deck, domain)
-        self.name = domain.name
 
     def set_mesh_grid(self, deck, domain):
-        self.nx = int(deck.doc["Mesh"][domain.name]["Number of Elements in X"])
-        self.ny = int(deck.doc["Mesh"][domain.name]["Number of Elements in Y"])
+        self.nx = domain.Number_of_Elements_in_X
+        self.ny = domain.Number_of_Elements_in_Y
         self.dx=domain.Lx/self.nx
         self.dy=domain.Ly/self.ny
         X, Y = np.meshgrid(np.arange(domain.x0, domain.x1, self.dx), np.arange(domain.y0, domain.y1, + self.dy))
@@ -16,4 +15,4 @@ class Mesh:
         Y=Y[:,1].copy()
         self.X = X
         self.Y = Y
-        self.ny1= int(self.ny/2)
+        self.M=np.zeros((domain.Number_of_Elements_in_X,domain.Number_of_Elements_in_Y))
