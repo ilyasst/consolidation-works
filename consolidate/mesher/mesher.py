3d8cf977f11d2ff21f450a723d92c1adc826ba36
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-18 14:00:53 -0400

    Update mesher.py
    created a new method to generate a "field" object

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from .mesh import Mesh
+from .fields import Field
 
 class Mesher():
 
     def __init__(self, deck, problem):
         self.mesh_domains(deck, problem)
+        self.initialize_fields(problem)
 
     def mesh_domains(self, deck, problem):
         self.meshes = []
         for domain in problem.domains:
             self.meshes.append( Mesh(deck, domain) )
+
+    def initialize_fields(self, problem):
+        self.fields =  []
+        for domain in problem.domains:
+            self.fields.append(Field( domain))
 
         
         
