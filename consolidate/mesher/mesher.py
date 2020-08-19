d074ccb60bed26bd0ae7e7fd77b11f1670ce80f7
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-19 16:13:20 -0400

    Update mesher.py

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from .mesh import Mesh
-from .fields import Field
 
 class Mesher():
 
     def __init__(self,  problem):
         self.mesh_domains( problem)
-        # self.initialize_empty_field(problem)
         self.initialize_fields(problem)
 # 
     def mesh_domains(self, problem):
@@ -18,23 +16,32 @@ class Mesher():
            
             
     def initialize_fields(self, problem):
-        for i, domain in enumerate(problem.domains):
-            # if domain.name == "Bottom Plate":
-            self.meshes[i].fields = []
-            for field in problem.required_fields:
-                if field == "Temperature":
-                    variable = domain.initial_temperature
-                    self.meshes[i].fields.append(Field(field, variable, domain))
-                elif field == "Thermal Conductivity X":
-                    variable = domain.material["Thermal Conductivity X"]
-                    self.meshes[i].fields.append(Field(field, variable, domain))
-                elif field == "Thermal Conductivity Y":
-                    variable = domain.material["Thermal Conductivity Y"]
-                    self.meshes[i].fields.append(Field(field, variable, domain))
-                elif field == "Density":
-                    variable = domain.material["Density"]
-                    self.meshes[i].fields.append(Field(field, variable, domain))
-                elif field == "Specific Heat":
-                    variable = domain.material["Specific Heat"]
-                    self.meshes[i].fields.append(Field(field, variable, domain))
+        
+        Fields = {}        
+        for field in problem.required_fields:
                 
+            for i, domain in enumerate(problem.domains): 
+                if i == 0:
+                    if field == "Temperature":
+                        Fields[field]=domain.initial_temperature*np.ones((self.meshes[i].nx, self.meshes[i].ny))    
+                    else:    
+                        try:
+                            Fields[field] = float(domain.material[field])*np.ones((self.meshes[i].nx, self.meshes[i].ny))
+                        except:
+                             continue
+                    continue
+                else:
+                    # import pdb; pdb.set_trace()
+                    if field == "Temperature":
+                
+                        Fields[field] = np.vstack((Fields[field] , domain.initial_temperature*np.ones((self.meshes[i].nx, self.meshes[i].ny))))
+                    else:
+                        try:
+                            Fields[field] = np.vstack((Fields[field] , float(domain.material[field])*np.ones((self.meshes[i].nx, self.meshes[i].ny))))
+                        except: 
+                            continue
+        self.fields=Fields
+                    
+                    
+                
+        
