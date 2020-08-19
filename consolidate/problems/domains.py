0e161397bdd67630ec056233a3994081f8aa70e4
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-19 17:28:26 -0400

    Update domains.py
    just included one extra information in problems

--- consolidate/problems/domains.py
+++ consolidate/problems/domains.py
@@ -1,6 +1,6 @@
 class RectangularDomain:
 
-    def __init__(self, name, corner0, corner1, material, initial_temperature, number_of_elements_X, number_of_elements_Y ):
+    def __init__(self, name, corner0, corner1, material, initial_temperature, number_of_elements_X, number_of_elements_Y,Power_Input_Density ):
         self.x0 = float(corner0[0])
         self.y0 = float(corner0[1])
         self.x1 = float(corner1[0])
@@ -12,6 +12,7 @@ class RectangularDomain:
         self.material = material
         self.name = name
         self.initial_temperature=initial_temperature
+        self.Power_Input_Density=Power_Input_Density
 
     def test(self, point):
         if point[0] > self.x0 and point[0] < self.x1 and point[1] > self.y0 and point[1] < self.y1:
