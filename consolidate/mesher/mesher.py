d1ddaac3953f226427324a757311d7caacc585c9
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-19 11:05:05 -0400

    Update mesher.py
    comments waived

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -15,28 +15,6 @@ class Mesher():
         for domain in problem.domains:
             self.meshes.append( Mesh(domain) )
 
-    # def initialize_empty_field(self, problem):
-    #     self.fields =  []
-    #     for i,domain in enumerate(problem.domains):
-    #         A=( Field(domain).M)
-    #         if i==0:
-    #             self.fields = A
-    #         else:
-    #             # import pdb; pdb.set_trace()
-    #             self.fields=np.concatenate((self.fields,A),axis=0)
-
-
-    # def initialize_fields(self, problem):
-    #     self.fields = []
-    
-    #     for domain in problem.domains:
-    #         initial_temperature= domain.initial_temperature
-    #         Kx = domain.material["Thermal Conductivity X"]
-    #         Ky = domain.material["Thermal Conductivity Y"]
-    #         Rho = domain.material["Density"]
-    #         Cp = domain.material["Specific Heat Capacity"]
-    #         # import pdb; pdb.set_trace()
-    #         self.fields.append(Field (domain, initial_temperature, Kx, Ky, Rho, Cp))
            
             
     def initialize_fields(self, problem):
@@ -60,16 +38,3 @@ class Mesher():
                     variable = domain.material["Specific Heat"]
                     self.meshes[i].fields.append(Field(field, variable, domain))
                 
-
-            
-        
-        
-# if deck_domain == "Bottom Plate":
-#                 corner0 = (0,0)
-#                 corner1 = self.geometry[deck_domain][0]
-#                 plate_material = deck.doc["Materials"][deck_domain]
-#                 plate_initial_temperature=float(deck.doc["Initial Conditions"][deck_domain]["Initial Temperature"])
-#                 number_of_elements_X = int(deck.doc["Mesh"][deck_domain]["Number of Elements in X"])
-#                 number_of_elements_Y = int(deck.doc["Mesh"][deck_domain]["Number of Elements in Y"])
-#                 self.domains.append(RectangularDomain(deck_domain, corner0, corner1, plate_material,plate_initial_temperature, number_of_elements_X, number_of_elements_Y ))
-            
