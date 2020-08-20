32764e0fcbfefb01bca07117fb6b14a727da818b
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-20 19:30:02 -0400

    Update mesh.py

--- consolidate/mesher/mesh.py
+++ consolidate/mesher/mesh.py
@@ -2,18 +2,18 @@ import numpy as np
 
 class Mesh:
 
-    def __init__(self, deck, domain):
-        self.set_mesh_grid(deck, domain)
+    def __init__(self,  domain):
+        self.set_mesh_grid(domain)
         self.name = domain.name
 
-    def set_mesh_grid(self, deck, domain):
-        self.nx = int(deck.doc["Mesh"][domain.name]["Number of Elements in X"])
-        self.ny = int(deck.doc["Mesh"][domain.name]["Number of Elements in Y"])
+    def set_mesh_grid(self, domain):
+        self.nx = domain.Number_of_Elements_in_X
+        self.ny = domain.Number_of_Elements_in_Y
         self.dx=domain.Lx/self.nx
         self.dy=domain.Ly/self.ny
-        X, Y = np.meshgrid(np.arange(domain.x0, domain.x0 + self.nx), np.arange(domain.y0, domain.y0 + self.ny))
-        X=X[1,:].copy()
-        Y=Y[:,1].copy()
+        X, Y = np.meshgrid(np.arange(domain.x0, domain.x1, self.dx), np.arange(domain.y0, domain.y1, + self.dy))
+        X=X[0,:].copy()
+        Y=Y[:,0].copy()
         self.X = X
         self.Y = Y
-        self.ny1= int(self.ny/2)
+        self.M=np.zeros((domain.Number_of_Elements_in_X,domain.Number_of_Elements_in_Y))
