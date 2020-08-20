83ea5a5301b2ce6c302caae164c987b392c85a99
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-20 14:42:23 -0400

    mesher now creates a matrix M with the final shape. mesher also calls Field

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -11,51 +11,18 @@ class Mesher():
 # 
     def mesh_domains(self, problem):
         self.meshes = []
+        nx=0
+        ny=0
         for domain in problem.domains:
             self.meshes.append( Mesh(domain) )
-
-          
-            
+            nx = max(nx,domain.Number_of_Elements_in_X)
+            ny = ny + domain.Number_of_Elements_in_Y
+        self.M= np.zeros((ny, nx))
+        
     def initialize_fields(self, problem):
-        
-        
-        fields = []
-
+        self.fields = {}
         for field_name in problem.required_fields:
-            for domain in problem.domains:
-                if field_name == "Temperature":
-                    value = (domain.initial_temperature*np.ones((domain.Number_of_Elements_in_Y, domain.Number_of_Elements_in_X)))
-                    if domain == problem.domains[0]:
-                        res=value
-                        value=[]
-                    else:
-                        res=np.concatenate((res, value))
-                        value=[]
-                   
-                if field_name == "Input Power Density":
-                    value=(domain.Power_Input_Density*np.ones((domain.Number_of_Elements_in_Y, domain.Number_of_Elements_in_X)))                     
-                    if domain == problem.domains[0]:
-                        res=value    
-                        value=[]
-                    else:
-                        res=np.concatenate((res, value))
-                        value=[]
-                elif field_name != "Temperature" or field_name!="Input Power Density":
-                    try:
-                        value=(float(domain.material[field_name])*np.ones((domain.Number_of_Elements_in_Y, domain.Number_of_Elements_in_X)))
-                    except:
-                        print ("missing " + field_name + " field")
-                        continue
-                    if domain == problem.domains[0]:
-                        res=value             
-                        value=[]
-                    else:
-                        res=np.concatenate((res, value))
-                        value=[]
-                
-            fields.append(Field(field_name, res))
-            self.fields=fields
-
+            self.fields[field_name] = Field(field_name, self.M, problem)
                         
 
                     
