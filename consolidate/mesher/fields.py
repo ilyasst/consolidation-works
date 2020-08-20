c877a4464470a0c574d8d184a84db2f89a173bb2
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-20 14:40:40 -0400

    Fields just receives the name, final shape matrix and problem

--- consolidate/mesher/fields.py
+++ consolidate/mesher/fields.py
@@ -3,6 +3,18 @@
 
             
 class Field:
-    def __init__(self, name, value):
+     def __init__(self, name, shape, problem):
         self.name = name
-        self.value = value
+        self.populate_field(shape, problem)
+        
+     def populate_field(self, shape, problem):
+         # import pdb; pdb.set_trace()
+         self.var = shape
+         for x_i, x_ in enumerate(self.var):
+             for y_i, y_ in enumerate(x_):
+                 for domain in problem.domains:
+                     self.var[x_i][y_i] = domain.initial_fields[self.name]
+                 
+                    
+                    
+      
