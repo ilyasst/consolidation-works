411715ccddf3f94b468223f3fba80236f33bc1d7
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-20 12:45:33 -0400

    method initialize_fields does all the calculations and calls Class Field to store the fields as objets

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -14,21 +14,50 @@ class Mesher():
         for domain in problem.domains:
             self.meshes.append( Mesh(domain) )
 
-           
-            
-    # def initialize_fields(self, problem):
-        
-    #     self.fields=[]
-    #     for field in problem.required_fields:                
-    #         for domain in problem.domains: 
-    #             self.fields.append (Field(field, domain))
-                    
+          
             
     def initialize_fields(self, problem):
         
-        self.fields=[]
-        self.fields.append (Field(problem))
         
+        fields = []
+
+        for field_name in problem.required_fields:
+            for domain in problem.domains:
+                if field_name == "Temperature":
+                    value = (domain.initial_temperature*np.ones((domain.Number_of_Elements_in_Y, domain.Number_of_Elements_in_X)))
+                    if domain == problem.domains[0]:
+                        res=value
+                        value=[]
+                    else:
+                        res=np.concatenate((res, value))
+                        value=[]
+                   
+                if field_name == "Input Power Density":
+                    value=(domain.Power_Input_Density*np.ones((domain.Number_of_Elements_in_Y, domain.Number_of_Elements_in_X)))                     
+                    if domain == problem.domains[0]:
+                        res=value    
+                        value=[]
+                    else:
+                        res=np.concatenate((res, value))
+                        value=[]
+                elif field_name != "Temperature" or field_name!="Input Power Density":
+                    try:
+                        value=(float(domain.material[field_name])*np.ones((domain.Number_of_Elements_in_Y, domain.Number_of_Elements_in_X)))
+                    except:
+                        print ("missing " + field_name + " field")
+                        continue
+                    if domain == problem.domains[0]:
+                        res=value             
+                        value=[]
+                    else:
+                        res=np.concatenate((res, value))
+                        value=[]
+                
+            fields.append(Field(field_name, res))
+            self.fields=fields
+
+                        
+
                     
                 
         
