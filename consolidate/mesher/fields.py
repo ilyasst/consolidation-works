9c0395a5a51e9ec56276bc6fae962671340c4805
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-20 16:11:16 -0400

    the class had a short problem... it was replacing the previous values with new ones.

--- consolidate/mesher/fields.py
+++ consolidate/mesher/fields.py
@@ -6,15 +6,20 @@ class Field:
      def __init__(self, name, shape, problem):
         self.name = name
         self.populate_field(shape, problem)
-        
+
+                    
      def populate_field(self, shape, problem):
          # import pdb; pdb.set_trace()
          self.var = shape
-         for x_i, x_ in enumerate(self.var):
-             for y_i, y_ in enumerate(x_):
-                 for domain in problem.domains:
-                     self.var[x_i][y_i] = domain.initial_fields[self.name]
-                 
-                    
-                    
-      
+         var=shape
+         nx=0
+         nyu=0
+         nyl=0
+         for domain in problem.domains:
+             nx=domain.Number_of_Elements_in_X
+             nyu=nyu+domain.Number_of_Elements_in_Y
+             var[nyl:nyu, 0:nx]=domain.initial_fields[self.name]
+             nyl=nyu
+         self.var=var
+             
+             
