98516f83d032be3c2340a80556fe718d2a6bcc09
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-20 17:08:51 -0400

    self.var and var receives now the VALUE of shape. This is crucial, otherwise all the fields receive the value of the last field

--- consolidate/mesher/fields.py
+++ consolidate/mesher/fields.py
@@ -6,12 +6,11 @@ class Field:
      def __init__(self, name, shape, problem):
         self.name = name
         self.populate_field(shape, problem)
-
+        
                     
      def populate_field(self, shape, problem):
-         # import pdb; pdb.set_trace()
-         self.var = shape
-         var=shape
+         self.var = shape.copy()
+         var=shape.copy()
          nx=0
          nyu=0
          nyl=0
@@ -20,6 +19,6 @@ class Field:
              nyu=nyu+domain.Number_of_Elements_in_Y
              var[nyl:nyu, 0:nx]=domain.initial_fields[self.name]
              nyl=nyu
-         self.var=var
+             self.var=var
              
              
