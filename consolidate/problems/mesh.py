c97cfee0dfa219594f2a18a69c54c68e66e515b2
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-24 16:23:36 -0400

    Create mesh.py

---
+++ consolidate/problems/mesh.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 14:39:01 2020
+
+@author: andre
+"""
+
+
+class DefineMesh:
+    def __init__(self, name, value,):
+        self.name = name
+        self.value=value
