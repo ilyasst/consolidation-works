65e019fbd1cb592283d5cd0481d1e3ad6cb58886
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-24 16:24:32 -0400

    Update in Domains. It now creates an object for each domain parameter

--- consolidate/problems/domains.py
+++ consolidate/problems/domains.py
@@ -1,27 +1,36 @@
+import numpy as np
+
 class RectangularDomain:
 
-    def __init__(self, name, corner0, corner1, material, initial_temperature, number_of_elements_X, number_of_elements_Y,Power_Input_Density, Initial_Dic ):
+    def __init__(self, name, corner0, corner1, MaterialProperties, InitialConditions, Mesh, BC, Geometry):
         self.x0 = float(corner0[0])
         self.y0 = float(corner0[1])
         self.x1 = float(corner1[0])
-        self.y1 = float(corner1[1])
-        self.Lx = self.x1 - self.x0
-        self.Ly = self.y1 - self.y0
-        self.Number_of_Elements_in_X = number_of_elements_X
-        self.Number_of_Elements_in_Y = number_of_elements_Y
-        self.material = material
+        self.y1 = float(corner1[1]) 
         self.name = name
-        self.initial_temperature=initial_temperature
-        self.Power_Input_Density=Power_Input_Density
-        self.Dic = Initial_Dic
-        self.initial_fields = {"Temperature": float(initial_temperature)}
+        self.MaterialProperties = MaterialProperties
+        self.InitialConditions = InitialConditions
+        self.Mesh = Mesh
+        self.BoundaryConditions=BC
+        self.Geometry = Geometry
+        
+       
 
-    def test_metric(self, point):
-        if point[0] > self.x0 and point[0] < self.x1 and point[1] > self.y0 and point[1] < self.y1:
+    def test(self, point):
+        if point[0] >= self.x0 and point[0] <= self.x1 and point[1] >= self.y0 and point[1] <= self.y1:
             return True
         else:
             return False
-        
+
+ 
+
     def set_field_init_value(self, field_dict):
+        
         for key, value in field_dict.items():
             self.initial_fields[key] = float(value)
+            
+    
+    def set_material_properties(self, mat):
+        import pdb; pdb.set_trace()
+        for key, value in mat:
+            self.MaterialProperties[key] = float (value)
