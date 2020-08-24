fdccc3ab6af344e88b010e8682e4ae96fce1b9c5
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-24 16:23:42 -0400

    Update initialconditions.py

--- consolidate/problems/initialconditions.py
+++ consolidate/problems/initialconditions.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 14:27:57 2020
+
+@author: andre
+"""
+
+
 class InitialConditions:
 
-    def __init__(self, domain, temperature):
-        self.Domain = domain
-        self.Temperature = temperature
+    def __init__(self, name, value,):
+        self.name = name
+        self.value=value
+        
