492bb756256fb04fc1f96e691f7159b560b642e7
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-26 13:21:54 -0400

    mesher now creates a unique object class Mesh contaning mask

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -1,38 +1,24 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from .mesh import Mesh
-# from .fields import Field
+from .fields import Field
 
 class Mesher():
 
     def __init__(self,  problem):
-        self.mesh_domains( problem)
+        # self.mesh_domains( problem)
+       
+        self.TotalNx = problem.TotalNx
+        self.TotalNy = problem.TotalNy
         self.initialize_fields(problem)
-# 
-    def mesh_domains(self, problem):
-        self.meshes = []
-        nx=0
-        ny=0
-        for domain in problem.domains:
-            self.meshes.append( Mesh(domain) )
-            nx = domain.mesh["Number of Elements in X"]
-            ny = ny + domain.mesh["Number of Elements in Y"]
-        # self.Total_elements_X = nx
-        # self.Total_elements_Y = ny
-        self.M= np.zeros((ny, nx))
-        
-    
-    
-    def initialize_fields(self, problem):
-        self.test=[]
-        # self.fields = {}
-        M=self.M.copy()
-        for domain in problem.domains:            
-            domain.generate_mask(M.copy())
-            domain.mask
+
             
-            self.test.append(domain.mask)
-            # import pdb; pdb.set_trace()
-                    
+            
+    def initialize_fields(self, problem):
+        self.meshes=[]
+        for domain in problem.domains:
+            self.meshes.append(Mesh( domain, self.TotalNx, self.TotalNy))
+            
+    
                 
         
