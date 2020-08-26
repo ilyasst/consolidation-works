5eccacf718d8f1a8fff33e7ea70959744c6062fe
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-26 13:22:11 -0400

    mask is now created in Mesh object

--- consolidate/mesher/mesh.py
+++ consolidate/mesher/mesh.py
@@ -2,10 +2,17 @@ import numpy as np
 
 class Mesh:
 
-    def __init__(self,  domain):
+    def __init__(self,  domain, totalNx=0, totalNy=0):
         import pdb; pdb.set_trace
         self.set_mesh_grid(domain)
         self.name = domain.name
+        # 
+        
+        if totalNx !=0 and totalNy !=0:
+            
+            self.M= np.zeros((totalNy,totalNx))
+            self.set_create_mask(domain)
+    
 
     def set_mesh_grid(self, domain):
         self.nx = domain.mesh["Number of Elements in X"]
@@ -17,3 +24,11 @@ class Mesh:
         Y=Y[:,0].copy()
         self.X = X
         self.Y = Y
+        
+    def set_create_mask(self, domain):
+        domain.generate_mask(self.M.copy())
+        initialmask=domain.mask
+        self.mask=initialmask
+        
+        
+  
