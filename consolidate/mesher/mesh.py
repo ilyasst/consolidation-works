baaab536ca29ceca93ff6bc80cca400e728fe1be
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-26 11:25:06 -0400

    changes to fit with the "yaml" modification.

--- consolidate/mesher/mesh.py
+++ consolidate/mesher/mesh.py
@@ -3,12 +3,13 @@ import numpy as np
 class Mesh:
 
     def __init__(self,  domain):
+        import pdb; pdb.set_trace
         self.set_mesh_grid(domain)
         self.name = domain.name
 
     def set_mesh_grid(self, domain):
-        self.nx = domain.Number_of_Elements_in_X
-        self.ny = domain.Number_of_Elements_in_Y
+        self.nx = domain.mesh["Number of Elements in X"]
+        self.ny = domain.mesh["Number of Elements in Y"]
         self.dx=domain.Lx/self.nx
         self.dy=domain.Ly/self.ny
         X, Y = np.meshgrid(np.arange(domain.x0, domain.x1, self.dx), np.arange(domain.y0, domain.y1, + self.dy))
@@ -16,4 +17,3 @@ class Mesh:
         Y=Y[:,0].copy()
         self.X = X
         self.Y = Y
-        self.M=np.zeros((domain.Number_of_Elements_in_X,domain.Number_of_Elements_in_Y))
