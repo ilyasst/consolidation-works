8d2f8a3d45966966e17bb8e0c9e33f156d905539
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-26 11:21:35 -0400

    included initial and last element position

--- consolidate/problems/domains.py
+++ consolidate/problems/domains.py
@@ -1,6 +1,7 @@
+import numpy as np
 class RectangularDomain:
 
-    def __init__(self, name, corner0, corner1, material, initialcondition, mesh ):
+    def __init__(self, name, corner0, corner1, ex0, ex1, ey0, ey1, material, initialcondition, mesh ):
         self.x0 = float(corner0[0])
         self.y0 = float(corner0[1])
         self.x1 = float(corner1[0])
@@ -12,12 +13,24 @@ class RectangularDomain:
         self.initial_condition = initialcondition.copy()
         self.mesh = mesh.copy()
         self.initial_fields = self.initial_condition.copy()
+        self.ex0=ex0
+        self.ex1= ex1
+        self.ey0 = ey0
+        self.ey1=ey1
 
     def test(self, point):
-        if point[0] > self.x0 and point[0] < self.x1 and point[1] > self.y0 and point[1] < self.y1:
+        if point[0] >= self.x0 and point[0] <= self.x1 and point[1] >= self.y0 and point[1] <= self.y1:
             return True
         else:
             return False
+        
+    def test_mesh(self, mesh):
+        if mesh[0] >= self.ey0 and mesh[0] <= self.ey1 and mesh[1] >= self.ex0 and mesh[1]<= self.ex1:
+            # import pdb; pdb.set_trace()
+            return True
+        else:
+            return False
+        
         
     def set_field_init_value(self, field_dict):
         for key, value in field_dict.items():
@@ -25,8 +38,11 @@ class RectangularDomain:
         
     def generate_mask(self, problem_M):
         self.mask = problem_M.copy()
-        for x_i, x_ in enumerate(problem_M):
-            for y_i, y_ in enumerate(problem_M):
+        for x_i in range (0,np.shape(problem_M)[0]):
+            for y_i in range (0,np.shape(problem_M)[1]):
                 # import pdb; pdb.set_trace()
-                if self.test( (x_i, y_i) ):
+                if self.test_mesh( (x_i, y_i) ):
                     self.mask[x_i][y_i] = 1
+                    
+                    
+                    
