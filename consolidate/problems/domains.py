4a0b8121c6ff97899506a88ba4fccfafdbd292dd
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-31 16:57:55 -0400

    removed an useless object

--- consolidate/problems/domains.py
+++ consolidate/problems/domains.py
@@ -10,14 +10,13 @@ class RectangularDomain:
         self.Ly = self.y1 - self.y0
         self.name = name
         self.material = material.copy()
-        self.initial_condition = initialcondition.copy()
         self.mesh = mesh.copy()
-        self.initial_fields = self.initial_condition.copy()
+        self.initial_fields = initialcondition.copy()
         self.ex0=ex0
         self.ex1= ex1
         self.ey0 = ey0
         self.ey1=ey1
-        self.Boundary_fields= boundarycond.copy()
+        self.boundary_fields= boundarycond.copy()
 
     def test(self, point):
         if point[0] >= self.x0 and point[0] <= self.x1 and point[1] >= self.y0 and point[1] <= self.y1:
@@ -40,7 +39,7 @@ class RectangularDomain:
         # import pdb; pdb.set_trace()
         for key, value in bc.items():
             
-            self.Boundary_fields[key] = float(value)
+            self.boundary_fields[key] = float(value)
             
         
         
