7ed3b7e98c5b2000e7a775be1ab466a09653db80
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-09-22 13:12:55 -0400

    modifications to accomodate the new objects containing x0,x1,y0, y1 and Lx,Ly respectively

--- consolidate/mesher/mesh.py
+++ consolidate/mesher/mesh.py
@@ -12,9 +12,9 @@ class Mesh:
     def set_mesh_grid(self, domain):
         self.nx = domain.mesh["Number of Elements in X"]
         self.ny = domain.mesh["Number of Elements in Y"]
-        self.dx=domain.Lx/self.nx
-        self.dy=domain.Ly/self.ny
-        X, Y = np.meshgrid(np.arange(domain.x0, domain.x1, self.dx), np.arange(domain.y0, domain.y1, + self.dy))
+        self.dx=domain.dimensions["Lx"]/self.nx
+        self.dy=domain.dimensions["Ly"]/self.ny
+        X, Y = np.meshgrid(np.arange(domain.corners["X"][0], domain.corners["X"][1], self.dx), np.arange(domain.corners["Y"][0], domain.corners["Y"][1], + self.dy))
         X=X[0,:].copy()
         Y=Y[:,0].copy()
         self.X = X
