0d24f38c35a26c0fd5dd0dc6379eff954a2f0685
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2021-01-04 11:35:58 -0500

    readme updated. Also, useless files and modules were deleted

--- consolidate/mesher/mesh.py
+++
@@ -1,7 +0,0 @@
-# import numpy as np
-
-# class Mesh:
-
-#     def __init__(self,  domain, totalNx=0, totalNy=0):
-#         self.set_mesh_grid(domain)
-#         self.name = domain.name
--- consolidate/problems/initialconditions.py
+++
@@ -1,5 +0,0 @@
-class InitialConditions:
-
-    def __init__(self, domain, temperature):
-        self.Domain = domain
-        self.Temperature = temperature
